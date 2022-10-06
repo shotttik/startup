7192629fc3f5b8984338608e0b05728589c27c55
shotttik <shota.akhlouri@gmail.com>
2022-10-06 16:51:20 +0400

    updated homepage, search input is ready

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,5 +1,37 @@
-const send_url = () => {
-    let url = document.querySelector('.c-form__input').value;
-    console.log(url);
+const check_url_web = (url) => {
+    const website_list = ['https://alta.ge/', 'https://zoommer.ge/', 'https://ultra.ge/']
+    for (const item of website_list) {
+        if (url.startsWith(item))
+            return true;
+    };
+    return false;
+};
+
+
+//setup before functions
+let typingTimer;                //timer identifier
+let doneTypingInterval = 200;  //time in ms 
+let myInput = document.querySelector('.search-form-input');
+
+//on keyup, start the countdown
+myInput.addEventListener('keyup', () => {
+    clearTimeout(typingTimer);
+    if (myInput.value) {
+        typingTimer = setTimeout(doneTyping, doneTypingInterval);
+    }
+});
+
+//user is "finished typing," do something
+function doneTyping() {
+    let url = document.querySelector('.search-form-input').value;
+    if (check_url_web(url)) {
+        document.querySelector('.search-form-btn').disabled = false;
+    };
 }
-document.querySelector('.c-form__button').addEventListener('click', send_url)
+const send_url = () => {
+    let url = document.querySelector('.search-form-input').value;
+    console.log(url);
+
+};
+document.querySelector('.search-form-btn').addEventListener('click', send_url)
+document.querySelector('.search-form-clear-btn').addEventListener('click', function () { document.querySelector('.search-form-input').value = ''; });
